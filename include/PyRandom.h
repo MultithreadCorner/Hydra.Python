9b48ef106e04b1abf85a66493ec239886595c296
Antonio Augusto Alves Junior <aalvesju@gmail.com>
2017-07-26 18:23:51 +0200

    some issues related to backend conflits ...

---
+++ include/PyRandom.h
@@ -0,0 +1,53 @@
+
+/*----------------------------------------------------------------------------
+ *
+ *   Copyright (C) 2017 Antonio Augusto Alves Junior
+ *                      
+ *
+ *   This file is part of Hydra.Python Analysis Framework.
+ *
+ *   Hydra is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Hydra is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Hydra.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *---------------------------------------------------------------------------*/
+
+/*
+ * PyRandom.h
+ *
+ *  Created on: Jul 26, 2017
+ *      Author: Antonio Augusto Alves Junior
+ */
+
+
+/**
+ * @file
+ * 
+ * @ingroup random
+ * 
+ * @brief This file implements macros and typedefs for hydra::Random
+ *
+ * @todo everything.
+ *
+ */
+
+
+#ifndef PYRANDOM_H_
+#define PYRANDOM_H_
+
+
+namespace hydra_python {
+
+
+}  // namespace hydra.python
+
+#endif /* PYRANDOM_H_ */
--- include/hydra.python/HydraPython.h
+++
@@ -1,4 +0,0 @@
-// the configured options
-#define HydraPython_VERSION_MAJOR 1
-#define HydraPython_VERSION_MINOR 0
-#define HydraPython_VERSION_PATCH 0
