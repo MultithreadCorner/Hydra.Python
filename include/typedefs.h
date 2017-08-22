1b7531b629c6bff11600074bc484d3cab951e019
Deepanshu <deepanshu2017@gmail.com>
2017-08-23 05:01:14 +0530

    Changed the licence, removed the PyIterator.h which I created for POC
    Signed-off-by: Deepanshu <deepanshu2017@gmail.com>

--- include/typedefs.h
+++ include/typedefs.h
@@ -2,22 +2,22 @@
 /*----------------------------------------------------------------------------
  *
  *   Copyright (C) 2017 Antonio Augusto Alves Junior
- *                      
  *
- *   This file is part of Hydra.Python Analysis Framework.
  *
- *   Hydra is free software: you can redistribute it and/or modify
+ *   This file is part of the Hydra.Python Analysis Framework.
+ *
+ *   Hydra.Python is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
  *   (at your option) any later version.
  *
- *   Hydra is distributed in the hope that it will be useful,
+ *   Hydra.Python is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details.
  *
  *   You should have received a copy of the GNU General Public License
- *   along with Hydra.  If not, see <http://www.gnu.org/licenses/>.
+ *   along with Hydra.Python.  If not, see <http://www.gnu.org/licenses/>.
  *
  *---------------------------------------------------------------------------*/
 
